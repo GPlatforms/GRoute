d2b6a6ce12b64c98fa98df3eeee82442ba3bd019
weibo <weibo@weibodeiMac.local>
2017-03-16 18:29:32 +0800

    feat(test):添加测试用例

--- GRoute_iOS/GRouteTests/GRouteTests.swift
+++ GRoute_iOS/GRouteTests/GRouteTests.swift
@@ -21,9 +21,31 @@ class GRouteTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testPredicate() {
+        let gRoute = GRoute.sharedInstance
+        XCTAssert(gRoute.textMatch(text: "asdf!", pattern: ".*"))
+        
+        XCTAssertEqual((gRoute.textMatch(text: "asdf", pattern: "x")), false)
+        
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testDownLoad() {
+        
+    }
+    
+    func testMatch() {
+        let gRoute = GRoute.sharedInstance
+        
+        gRoute.routeConfig = [Rule(newReg: "fa", newURL: "http://www.baidu.com"),
+                              Rule(newReg: ".*", newURL: "http://www.taobao.com")]
+        let urlA = gRoute.match(functionName: "fa")
+        let urlB = gRoute.match(functionName: "fb")
+        print(urlA)
+        print(urlB)
+        XCTAssertEqual(urlA, "http://www.baidu.com")
+        XCTAssertEqual(urlB, "http://www.taobao.com")
     }
     
     func testPerformanceExample() {
