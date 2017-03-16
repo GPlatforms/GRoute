4cb3a9b3b51b031ce2156113c9796b4bf67b321f
weibo <weibo@weibodeiMac.local>
2017-03-16 15:22:15 +0800

    feat(product):增加从服务器请求api

--- GRoute_iOS/GRoute/GRoute.swift
+++ GRoute_iOS/GRoute/GRoute.swift
@@ -7,7 +7,63 @@
 //
 
 import Foundation
+import Alamofire
+
+enum GRouteResult {
+    case Success([String:Any])
+    case Fail(Error?)
+}
 
 class GRoute {
+    static let sharedInstance = GRoute()
     
+    init() {
+        print(GRoute.sharedInstance.match(text: "adsfa!", pattern: ".*"))
+    }
+    
+    private(set) var routeConfig:[String:Any] = [:]
+    
+    public func getRouteConfigFromServer(_ url: String,
+                                         method: HTTPMethod = .get,
+                                         parameters: Parameters? = nil,
+                                         encoding: ParameterEncoding = URLEncoding.default,
+                                         headers: HTTPHeaders? = nil,
+                                         callback:@escaping ((GRouteResult) -> Void))  {
+        Alamofire.request(url).responseJSON { response in
+            debugPrint("Request: \(response.request)")
+            debugPrint("Response: \(response.response)")
+            debugPrint("Error: \(response.error)")
+
+            
+            if let json = response.result.value {
+                debugPrint("JSON: \(json)")
+            }
+            switch response.result {
+            case .success(_):
+                var res:[String:Any] = [:]
+                callback(GRouteResult.Success(res))
+                break
+            case .failure(_):
+                callback(GRouteResult.Fail(response.error))
+                break
+            }
+        }
+    }
+    
+    
+    
+    public func match(moduleName:String) -> String {
+        for (key,value) in self.routeConfig {
+            if match(text: moduleName, pattern: key) {
+                return ""
+            }
+        }
+        return ""
+    }
+    
+    public func match(text: String, pattern: String) -> Bool {
+        return NSPredicate(format: "SELF MATCHES %@", pattern).evaluate(with: text)
+    }
 }
+
+let GRouteClient = GRoute.sharedInstance
---
+++ GRoute_iOS/Pods/Target Support Files/Pods-GRoute/Pods-GRoute-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GRoute : NSObject
+@end
+@implementation PodsDummy_Pods_GRoute
+@end
